7cf02ff1115e3e5e437faf962614b5143b175523
McCrearyD <mccreary@dyllan.ai>
2021-08-09 12:21:32 -0700

    minor tweaks

--- create_all.sh
+++ create_all.sh
@@ -7,7 +7,12 @@ for i in \
     2.0.3 \
     2.0.4
 do
-    echo "\ninstalling $i"
+    echo "\ninstalling $i..."
     pip install hub==$i >> /dev/null 2>&1
     python create.py
 done
+
+echo "\nfinished creating datasets for all versions!\n"
+
+echo "uninstalling hub..."
+pip uninstall hub -y >> /dev/null 2>&1
