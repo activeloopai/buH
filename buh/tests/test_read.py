2ee9dd3b19461e8c4648a3af2bd1c7e28801c1c3
McCrearyD <mccreary@dyllan.ai>
2021-08-09 16:01:05 -0700

    run 2.0.1 test and skip others

--- buh/tests/test_read.py
+++ buh/tests/test_read.py
@@ -1,31 +1,33 @@
+import pytest
 import numpy as np
 from buh.util import *
 from buh.constants import *
 
-def _load0():
-    dataset_path = get_dataset_path("2_0_1")
+def _load0(dataset_path):
     return hub.Dataset(dataset_path)
 
-def _load_v2_0_1():
+def _load_v2_0_1(dataset_path):
     assert UNDERSCORED_VERSION == "2_0_1"
-    return _load0()
+    return _load0(dataset_path)
 
 
-def _load_v2_0_2():
+def _load_v2_0_2(dataset_path):
     assert UNDERSCORED_VERSION == "2_0_2"
-    return _load_v2_0_1()
+    return _load_v2_0_1(dataset_path)
 
 
-def _load_v2_0_3():
+def _load_v2_0_3(dataset_path):
     raise NotImplementedError
 
 
-def _load_v2_0_4():
+def _load_v2_0_4(dataset_path):
     raise NotImplementedError
  
  
-def _load_dataset():
-    return eval(f"_load_v{UNDERSCORED_VERSION}()")
+def _load_dataset(underscored_version):
+    # TODO: docstring
+    dataset_path = get_dataset_path(underscored_version)
+    return eval(f"_load_v{UNDERSCORED_VERSION}(\"{dataset_path}\")")
 
 
 def _assert_valid(ds):
@@ -34,25 +36,34 @@ def _assert_valid(ds):
 
     np.testing.assert_array_equal(images, get_images())
     np.testing.assert_array_equal(labels, get_labels())
- 
- 
-# don't parametrize these tests (in case there are API changes)
 
+
+def _skip_if_not_available(required_version):
+    """Calls pytest.skip if the current hub version is < required_version"""
+
+    if hub.__version__ < required_version:
+        pytest.skip()
+ 
+ 
 def test_v2_0_1():
-    ds = _load_dataset()
+    _skip_if_not_available("2.0.1")
+    ds = _load_dataset("2_0_1")
     _assert_valid(ds)
 
 
 def test_v2_0_2():
-    ds = _load_dataset()
+    _skip_if_not_available("2.0.2")
+    ds = _load_dataset("2_0_2")
     _assert_valid(ds)
 
 
 def test_v2_0_3():
-    ds = _load_dataset()
+    _skip_if_not_available("2.0.3")
+    ds = _load_dataset("2_0_3")
     _assert_valid(ds)
 
 
 def test_v2_0_4():
-    ds = _load_dataset()
+    _skip_if_not_available("2.0.4")
+    ds = _load_dataset("2_0_4")
     _assert_valid(ds)
