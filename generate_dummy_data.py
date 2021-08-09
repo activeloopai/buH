767105990dbfa706f9fb7b7e215fcdad63e52932
McCrearyD <mccreary@dyllan.ai>
2021-08-09 12:18:14 -0700

    fix random seed

--- generate_dummy_data.py
+++ generate_dummy_data.py
@@ -3,8 +3,10 @@ import numpy as np
 
 def get_images():
     # ~39MB
+    np.random.seed(0)
     return np.random.randint(255, size=(50000, 28, 28), dtype=np.uint8)
 
 
 def get_labels():
+    np.random.seed(0)
     return np.random.randint(2, size=50000, dtype=np.uint8)
