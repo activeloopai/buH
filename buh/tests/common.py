b40130ad61ad8f699c9d57570421f6fe3e549b97
McCrearyD <mccreary@dyllan.ai>
2021-08-09 16:39:44 -0700

    test same version too

--- buh/tests/common.py
+++ buh/tests/common.py
@@ -6,7 +6,7 @@ versions = pytest.mark.parametrize("version", ALL_VERSIONS)
 
 
 def skip_if_not_available(required_version):
-    """Calls pytest.skip if the current hub version is <= required_version"""
+    """Calls pytest.skip if the current hub version is < required_version"""
 
-    if hub.__version__ <= required_version:
+    if hub.__version__ < required_version:
         pytest.skip()
