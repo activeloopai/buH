48f272456590ac48c07260c20f33407f6acbdd34
McCrearyD <mccreary@dyllan.ai>
2021-08-12 12:38:20 -0700

    write update samples test

--- buh/tests/test_write.py
+++ buh/tests/test_write.py
@@ -49,3 +49,23 @@ def test_new_tensor(version, request):
     ds.new_tensor.extend(get_images()[:10])
 
     assert ds.meta.version == hub.__version__
+
+
+@versions
+def test_update_samples(version, request):
+    try_skipping(request)
+    skip_if_not_available(version)
+    ds = load_dataset_copy(version, overwrite=True)
+
+    ds[IMAGES][10:20] = get_images()[:10]
+    ds[LABELS][10:30] = get_labels()[:20]
+
+    assert_new_versions(ds)
+
+    actual_images = ds[IMAGES].numpy()
+    actual_labels = ds[LABELS].numpy()
+    expected_images = np.concatenate((get_images()[:10], get_images()[:10], get_images()[20:]))
+    expected_labels = np.expand_dims(np.concatenate((get_labels()[:10], get_labels()[:20], get_labels()[30:])), axis=-1)
+
+    np.testing.assert_array_equal(actual_images, expected_images)
+    np.testing.assert_array_equal(actual_labels, expected_labels)
