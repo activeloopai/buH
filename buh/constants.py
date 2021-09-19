34c2abd70d296c47a7b94de18ea8fe58d283a0de
Davit Buniatyan <david.buniatyan@gmail.com>
2021-09-19 10:50:43 -0700

    Update constants.py

--- buh/constants.py
+++ buh/constants.py
@@ -14,6 +14,7 @@ ALL_VERSIONS = [
     "2.0.7",
     "2.0.8",
     "2.0.9",
+    "2.0.11"
 ]  # TODO use inside the .sh script
 
 # the staging hub version is the version that will be the next release
