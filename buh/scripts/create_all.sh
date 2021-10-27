18e2b40102e08c58b163ea016d1f7479eccf5c00
Davit Buniatyan <david.buniatyan@gmail.com>
2021-10-26 21:26:36 -0700

    Update create_all.sh
    filter out releases which don't have specific branch.

--- buh/scripts/create_all.sh
+++ buh/scripts/create_all.sh
@@ -8,21 +8,15 @@ BASEDIR=$(dirname $0)
 SCRIPT=$BASEDIR/../create_current_version.py
 
 for i in \
-    2.0.2 \
-    2.0.3 \
     2.0.4 \
-    2.0.5 \
     2.0.6 \
     2.0.7 \
     2.0.8 \
-    2.0.9 \
     2.0.11 \
     2.0.12 \
-    2.0.13 \
     2.0.14
 do
     echo "\ninstalling hub version $i..."
-    # python3 -m pip install hub==$i || python -m pip install hub==$i
     python3 -m pip install git+https://github.com/activeloopai/Hub.git@release/$i || python -m pip install git+https://github.com/activeloopai/Hub.git@release/$i
     echo "creating dataset for hub version $i"
     python3 $SCRIPT || python $SCRIPT
