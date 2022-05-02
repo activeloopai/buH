6063a7c99aab671eefac812ffea707f45d151bc3
Davit Buniatyan <david.buniatyan@gmail.com>
2022-05-02 10:34:24 -0700

    Update constants.py

--- buh/constants.py
+++ buh/constants.py
@@ -22,14 +22,15 @@ ALL_VERSIONS = [
     "2.3.3",
     "2.3.4",
     "2.3.5",
-    "2.4.0"
+    "2.4.0",
+    "2.4.1"
 ]  # TODO use inside the .sh script
 
 # the staging hub version is the version that will be the next release
 # this should be updated when the staging hub is released.
 # IMPORTANT: after updating this version, update `ALL_VERSIONS` and `create_all.sh`!
 # TODO: automate this
-STAGING_HUB_VERSION = "2.4.1"
+STAGING_HUB_VERSION = "2.4.2"
 
 COMPRESSION = None
 IMAGES = "images"
