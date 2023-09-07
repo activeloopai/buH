47c77def9cea148868307545618b07bb371e069f
Fayaz Rahman <fayazrahman4u@gmail.com>
2023-09-07 16:19:30 +0530

    Update constants.py

--- buh/constants.py
+++ buh/constants.py
@@ -25,14 +25,14 @@ ALL_VERSIONS = [
     "3.4.4",
     "3.5.4",
     # Replace this with the new patch version
-    "3.6.23"
+    "3.6.24"
 ]  # TODO use inside the .sh script
 
 # the staging deeplake version is the version that will be the next release
 # this should be updated when the staging deeplake is released.
 # IMPORTANT: after updating this version, update `ALL_VERSIONS` and `create_all.sh`!
 # TODO: automate this
-STAGING_HUB_VERSION = "3.6.24"
+STAGING_HUB_VERSION = "3.6.25"
 
 COMPRESSION = None
 IMAGES = "images"
