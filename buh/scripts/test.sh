cea238b03b0ebeea2784617e48c2cef2dde94fd5
artgish <artyom@activeloop.dev>
2024-06-20 08:55:54 +0400

    wip

--- buh/scripts/test.sh
+++ buh/scripts/test.sh
@@ -1,14 +1,5 @@
 #!/bin/bash
 
-if ( hash python3 ); then
-  PYTHON=python3
-elif ( hash python); then
-  PYTHON=python
-else
-  echo "Neither python or python3 is found in PATH"
-  exit 1
-fi
-
 readarray -t versions <<< "$($PYTHON -c '
 from buh.constants import ALL_VERSIONS
 for i in ALL_VERSIONS:
@@ -18,6 +9,6 @@ for i in ALL_VERSIONS:
 for i in "${versions[@]}"; do
     # shellcheck source=/dev/null
     source "venv_$i"
-    python3 -m pytest --junitxml="buh.$i.results.xml" --capture=sys -o junit_logging=all buH/
+    pytest --junitxml="buh.$i.results.xml" --capture=sys -o junit_logging=all buH/
     deactivate
 done
